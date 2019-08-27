e10ace180342ff9a535e76d79b9bfa53ed62ba24
duc <ducxo98@gmail.com>
2019-08-27 11:01:29 +0700

    commit

---
+++ var.js
@@ -0,0 +1,22 @@
+// function main() {
+//   console.log("START");
+//   if (true) {
+//     var lang = "vi";
+//     let target = "en-us";
+//     console.log("inside block");
+//     console.log(target);
+//   }
+//   console.log(lang);
+//   console.log(target);
+// }
+function main() {
+    var x = 5;
+    console.log(x);
+    var x = 10;
+    console.log(x);
+    var y = 55;
+    console.log(y);
+    var y = 100;
+    console.log(y);
+}
+main();
